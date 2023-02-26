d89683d5d1ce291a0dfba1de44f10e94536fef74
Moonraker <zxkst2@protonmail.com>
2023-02-26 04:50:56 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import {
   Box,
   Center,
+  Environment,
+  Float,
   OrbitControls,
+  PresentationControls,
+  Sparkles,
   Text3D,
   useMatcapTexture,
 } from "@react-three/drei";
@@ -43,42 +47,49 @@ const Text = () => {
   const [matcapTexture] = useMatcapTexture("46804D_CBE9AC_90B57C_95D38F", 256);
   return (
     <>
-      <group position={[0, 2, 0]}>
-        <Center top>
-          <Text3D
-            font={"/font.typeface.json"}
-            size={0.75}
-            height={0.2}
-            curveSegments={12}
-            bevelEnabled
-            bevelThickness={0.04}
-            bevelSize={0.04}
-            bevelOffset={0}
-            bevelSegments={5}
-          >
-            MooNRakeR
-            <meshMatcapMaterial matcap={matcapTexture} />
-          </Text3D>
-        </Center>
-      </group>
-      <group position={[0, -2, 0]}>
-        <Center bottom>
-          <Text3D
-            font={"/font.typeface.json"}
-            size={0.75}
-            height={0.2}
-            curveSegments={12}
-            bevelEnabled
-            bevelThickness={0.04}
-            bevelSize={0.04}
-            bevelOffset={0}
-            bevelSegments={5}
-          >
-            WeB DeV
-            <meshMatcapMaterial matcap={matcapTexture} />
-          </Text3D>
-        </Center>
-      </group>
+      <Float
+        speed={5} // Animation speed, defaults to 1
+        rotationIntensity={0.3} // XYZ rotation intensity, defaults to 1
+        floatIntensity={0.5} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+        floatingRange={[-0.1, 0.1]}
+      >
+        <group position={[0, 2, 0]}>
+          <Center top>
+            <Text3D
+              font={"/font.typeface.json"}
+              size={0.75}
+              height={0.2}
+              curveSegments={12}
+              bevelEnabled
+              bevelThickness={0.04}
+              bevelSize={0.04}
+              bevelOffset={0}
+              bevelSegments={5}
+            >
+              MooNRakeR
+              <meshMatcapMaterial matcap={matcapTexture} />
+            </Text3D>
+          </Center>
+        </group>
+        <group position={[0, -2, 0]}>
+          <Center bottom>
+            <Text3D
+              font={"/font.typeface.json"}
+              size={0.75}
+              height={0.2}
+              curveSegments={12}
+              bevelEnabled
+              bevelThickness={0.04}
+              bevelSize={0.04}
+              bevelOffset={0}
+              bevelSegments={5}
+            >
+              WeB DeV
+              <meshMatcapMaterial matcap={matcapTexture} />
+            </Text3D>
+          </Center>
+        </group>
+      </Float>
     </>
   );
 };
@@ -86,10 +97,31 @@ const Text = () => {
 function App() {
   return (
     <Canvas>
-      <OrbitControls />
-      <directionalLight intensity={0.5} />
-      <Cube />
-      <Text />
+      {/* <OrbitControls /> */}
+      <PresentationControls
+        enabled={true} // the controls can be disabled by setting this to false
+        global={false} // Spin globally or by dragging the model
+        cursor={true} // Whether to toggle cursor style on drag
+        snap={false} // Snap-back to center (can also be a spring config)
+        speed={1} // Speed factor
+        zoom={1} // Zoom factor when half the polar-max is reached
+        rotation={[0, 0, 0]} // Default rotation
+        polar={[0, 0]} // Vertical limits
+        azimuth={[-Infinity, Infinity]} // Horizontal limits
+        config={{ mass: 1, tension: 170, friction: 26 }} // Spring config
+        // The DOM element events for this controller will attach to
+      >
+        <directionalLight intensity={0.5} />
+        <Float
+          speed={1} // Animation speed, defaults to 1
+          rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+          floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+          floatingRange={[-1, 1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+        >
+          <Cube />
+        </Float>
+        <Text />
+      </PresentationControls>
     </Canvas>
   );
 }
