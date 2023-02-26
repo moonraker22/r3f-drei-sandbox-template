3573f84ed2b4e7254df8961a4b746de24c1981be
Moonraker <zxkst2@protonmail.com>
2023-02-26 03:01:58 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,85 @@
 import {
   Box,
   Center,
-  Text3D,
   OrbitControls,
+  Text3D,
   useMatcapTexture,
 } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Group } from "three";
 import "./App.css";
 import "./index.css";
 
-const Scene = () => {
+const Cube = () => {
   const scene = useRef<Group>(null!);
+  const [hovered, setHovered] = useState(false);
   useFrame(() => {
     if (!scene.current) return;
     scene.current.rotation.y += 0.04;
     scene.current.rotation.x += 0.04;
     scene.current.rotation.z += 0.04;
   });
+
   return (
     <group ref={scene}>
-      <Box>
-        <meshLambertMaterial attach="material" color="white" />
+      <Box
+        onPointerEnter={() => setHovered(!hovered)}
+        onPointerLeave={() => setHovered(!hovered)}
+        scale={hovered ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      >
+        <meshLambertMaterial
+          attach="material"
+          color={hovered ? "green" : "white"}
+        />
       </Box>
+      <directionalLight intensity={hovered ? 0.9 : 0.3} position={[0, 2, 3]} />
     </group>
   );
 };
 
 const Text = () => {
+  // Matcaps from https://github.com/nidorx/matcaps
   const [matcapTexture] = useMatcapTexture("46804D_CBE9AC_90B57C_95D38F", 256);
   return (
-    <group position={[0, 2, 0]}>
-      <Center top>
-        <Text3D
-          font={"/font.typeface.json"}
-          size={0.75}
-          height={0.2}
-          curveSegments={12}
-          bevelEnabled
-          bevelThickness={0.04}
-          bevelSize={0.04}
-          bevelOffset={0}
-          bevelSegments={5}
-        >
-          MooNRakeR
-          <meshMatcapMaterial matcap={matcapTexture} />
-        </Text3D>
-      </Center>
-    </group>
+    <>
+      <group position={[0, 2, 0]}>
+        <Center top>
+          <Text3D
+            font={"/font.typeface.json"}
+            size={0.75}
+            height={0.2}
+            curveSegments={12}
+            bevelEnabled
+            bevelThickness={0.04}
+            bevelSize={0.04}
+            bevelOffset={0}
+            bevelSegments={5}
+          >
+            MooNRakeR
+            <meshMatcapMaterial matcap={matcapTexture} />
+          </Text3D>
+        </Center>
+      </group>
+      <group position={[0, -2, 0]}>
+        <Center bottom>
+          <Text3D
+            font={"/font.typeface.json"}
+            size={0.75}
+            height={0.2}
+            curveSegments={12}
+            bevelEnabled
+            bevelThickness={0.04}
+            bevelSize={0.04}
+            bevelOffset={0}
+            bevelSegments={5}
+          >
+            WeB DeV
+            <meshMatcapMaterial matcap={matcapTexture} />
+          </Text3D>
+        </Center>
+      </group>
+    </>
   );
 };
 
@@ -57,7 +88,7 @@ function App() {
     <Canvas>
       <OrbitControls />
       <directionalLight intensity={0.5} />
-      <Scene />
+      <Cube />
       <Text />
     </Canvas>
   );
