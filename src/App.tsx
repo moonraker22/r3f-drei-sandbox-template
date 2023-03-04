705248006102763b5ce982b4580971c3cf61ae3d
Moonraker <zxkst2@protonmail.com>
2023-03-04 11:57:47 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,85 @@
-import {
-  Box,
-  Center,
-  OrbitControls,
-  Text3D,
-  useMatcapTexture,
-} from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef, useState } from "react";
-import { Group } from "three";
+import { Center, OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
+import { Group, TorusKnotGeometry } from "three";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
+import { createSculpture, createSculptureWithGeometry } from "shader-park-core";
+import { spCode, spText } from "./spCode";
 import "./App.css";
 import "./index.css";
 
-const Cube = () => {
+const Torus = () => {
   const scene = useRef<Group>(null!);
-  const [hovered, setHovered] = useState(false);
+  // const [hovered, setHovered] = useState(false);
+
+  let geometry = new TorusKnotGeometry(10, 3, 192, 96);
+  geometry.computeBoundingSphere();
+  geometry.center();
+
+  let params = { time: 0 };
+
+  let torus = createSculptureWithGeometry(geometry, spCode, () => ({
+    time: params.time,
+  }));
+
   useFrame(() => {
     if (!scene.current) return;
     scene.current.rotation.y += 0.04;
     scene.current.rotation.x += 0.04;
     scene.current.rotation.z += 0.04;
+    params.time += 0.01;
   });
 
   return (
     <group ref={scene}>
-      <Box
-        onPointerEnter={() => setHovered(!hovered)}
-        onPointerLeave={() => setHovered(!hovered)}
-        scale={hovered ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      >
-        <meshLambertMaterial
-          attach="material"
-          color={hovered ? "green" : "white"}
-        />
-      </Box>
-      <directionalLight intensity={hovered ? 0.9 : 0.3} position={[0, 2, 3]} />
+      <primitive object={torus} position={[0, 0, 0]} scale={[0.1, 0.1, 0.1]} />
     </group>
   );
 };
 
 const Text = () => {
-  // Matcaps from https://github.com/nidorx/matcaps
-  const [matcapTexture] = useMatcapTexture("46804D_CBE9AC_90B57C_95D38F", 256);
+  const loader = new FontLoader();
+  const three = useThree();
+
+  let params = { time: 0 };
+
+  useFrame(() => {
+    params.time += 0.01;
+  });
+
+  loader.load("/font.typeface.json", function (font) {
+    let geometry = new TextGeometry("MooNRakeR", {
+      font: font,
+      size: 1,
+      height: 0.1,
+      curveSegments: 12,
+      bevelEnabled: true,
+      bevelThickness: 0.01,
+      bevelSize: 0.1,
+      bevelOffset: 0,
+      bevelSegments: 1,
+    });
+    geometry.computeBoundingSphere();
+    geometry.center();
+
+    let mesh = createSculptureWithGeometry(geometry, spText, () => ({
+      time: params.time,
+      size: 15,
+      gyroidSteps: 0.01,
+    }));
+
+    mesh.position.set(0, 2.3, 0);
+
+    const group = new Group();
+    group.add(mesh);
+
+    three.scene.add(group);
+  });
+
   return (
     <>
-      <group position={[0, 2, 0]}>
-        <Center top>
-          <Text3D
-            font={"/font.typeface.json"}
-            size={0.75}
-            height={0.2}
-            curveSegments={12}
-            bevelEnabled
-            bevelThickness={0.04}
-            bevelSize={0.04}
-            bevelOffset={0}
-            bevelSegments={5}
-          >
-            MooNRakeR
-            <meshMatcapMaterial matcap={matcapTexture} />
-          </Text3D>
-        </Center>
-      </group>
-      <group position={[0, -2, 0]}>
-        <Center bottom>
-          <Text3D
-            font={"/font.typeface.json"}
-            size={0.75}
-            height={0.2}
-            curveSegments={12}
-            bevelEnabled
-            bevelThickness={0.04}
-            bevelSize={0.04}
-            bevelOffset={0}
-            bevelSegments={5}
-          >
-            WeB DeV
-            <meshMatcapMaterial matcap={matcapTexture} />
-          </Text3D>
-        </Center>
-      </group>
+      <group position={[0, 2, 0]}></group>
     </>
   );
 };
@@ -87,9 +88,10 @@ function App() {
   return (
     <Canvas>
       <OrbitControls />
-      <directionalLight intensity={0.5} />
-      <Cube />
-      <Text />
+      <Center>
+        <Torus />
+        <Text />
+      </Center>
     </Canvas>
   );
 }
---
+++ src/spCode.ts
@@ -0,0 +1,32 @@
+// @ts-nocheck
+export function spCode() {
+  displace(getRayDirection());
+  rotateY(-0.1 * time);
+  let n = fractalNoise(getSpace() * 50 + time + 10000000);
+  color(vec3(0.1, 0.3, 0.1) + normal * 0.2);
+  metal(n);
+  shine(n);
+  sphere(0.5 + n * nsin(time) * 3);
+}
+
+export function spText() {
+  let size = input(12, 10, 50.0);
+  let gyroidSteps = input(0.06, 0, 0.1);
+  let s = getSpace();
+  displace(getRayDirection());
+  let col = vec3(0.8, 1.2, 0.8) + normal * 0.2;
+  metal(0.2);
+  shine(0.5);
+
+  col -= length(s) * 0.7;
+  color(col);
+
+  s = vec3(s.x, s.y, s.z - time);
+  let sdf = min(
+    gyroidSteps,
+    sin(s.x * size) + sin(s.y * size) + sin(s.z * size)
+  );
+  setSDF(sdf);
+  intersect();
+  sphere(1);
+}
